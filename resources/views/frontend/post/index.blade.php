5f85fa665e5eca5a871178c45d0fa38aeaeb2efe
Bhavithaseva <bhavithaseva@gmail.com>
2022-10-21 12:18:48 +0530

    Laravel 8 Blog - 15: View all Posts by Category in Laravel 8 and Fetch Category data in Navbar Link

---
+++ resources/views/frontend/post/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+@section('content')
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="category-heading">
+                    <h4>{{$category->name}}</h4>
+                </div>
+
+                @forelse ($post as $postitem)
+             
+                <div class="card card-shadow mt-4">
+                    <div class="card-body">
+                        <a href="{{url('tutorial/'.$category->slug.'/'.$postitem->slug)}}" class="text-decoration-none">
+                        <h2 class="post-heading">{{$postitem->name}}</h2>
+                        </a>
+                        <h6>Posted On: {{$postitem->created_at->format('d-m-y')}}</h6>
+                        <span class="ms-3">Posted On: {{$postitem->user->name}}</span>
+                    </div>
+                </div>
+                @empty
+                <div class="card card-shadow mt-4">
+                    <div class="card-body">
+                        <h1>No Post Available</h1>
+                    </div>
+
+                </div>
+                @endforelse
+            </div>
+            <div class="col-md-3">
+               <div class="border p-2">
+                 <h4>Advertising Area</h4>
+               </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
--- resources/views/layouts/inc/frontend-navbar.blade.php
+++ resources/views/layouts/inc/frontend-navbar.blade.php
@@ -12,34 +12,40 @@
         </div>
     </div>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-green">
-  <a class="container" ></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home </a>
-      </li>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-green">
+  <div class="container">
+   
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link"  href="{{url('/')}}">Home</a>
+        </li>
+        {{-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li> --}}
+         @php 
+      $categories = App\Models\Category::where('navbar_status','0')->where('status','0')->get();
+      @endphp 
+      @foreach($categories as $cateitem)
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="{{url('tutorial/'.$cateitem->slug)}}">{{$cateitem->name}}</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-           <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    
-    </ul>
-    
+      @endforeach
+      </ul>
+      
+    </div>
   </div>
 </nav>
+
 </div>
