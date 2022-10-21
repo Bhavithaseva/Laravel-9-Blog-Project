4a04c0fb9f81526ccb22f5c1ccab6199f9dd8d8a
Bhavithaseva <bhavithaseva@gmail.com>
2022-10-21 15:07:32 +0530

    Laravel 8 Blog - 19: How to add Latest Post List in Laravel 8 | get data in Descending order laravel

--- resources/views/frontend/post/view.blade.php
+++ resources/views/frontend/post/view.blade.php
@@ -8,7 +8,7 @@
 <div class="py-4">
     <div class="container">
         <div class="row">
-            <div class="col-md-9">
+            <div class="col-md-8">
                 <div class="category-heading">
                     <h4 class="mb-0">{!! $post->name !!}</h4>
                 </div>
@@ -25,7 +25,7 @@
                 </div>
 
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class="border p-2 my-2">
                     <h4>Advertising Area</h4>
                 </div>
@@ -34,6 +34,19 @@
                 </div>
                 <div class="border p-2 my-2">
                     <h4>Advertising Area</h4>
+                </div>
+                <div class="card mt-3">
+                    <div class="card-header">
+                        <h4>Latest Posts</h4>
+                    </div>
+                    <div class="card-body">
+                        @foreach($latest_posts as $latest_post_item)
+                        <a href="{{url('tutorial/'.$latest_post_item->category->slug.'/'.$latest_post_item->slug)}}"
+                            class="text-decoration-none">
+                            <h6>{{$latest_post_item->name}}</h6>
+                        </a>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
