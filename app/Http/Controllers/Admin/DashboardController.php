7712f1462ae8553765da247bcd83e6f974ab6a08
Bhavithaseva <bhavithaseva@gmail.com>
2022-10-22 12:42:10 +0530

    Laravel 8 Blog - 25: Count data from database in Laravel 8 | How to make statistics in Laravel 8

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $categories = Category::count();
+        $posts=Post::count();
+        $users=User::where('role_as','0')->count();
+        $admin=User::where('role_as','1')->count();
+        return view('admin.dashboard', compact('categories','posts','users','admin'));
     }
 }
